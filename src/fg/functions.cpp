823503a3b0152e832d74510a3c731400ede3bd86
joinmc <joinmc@localhost>
2011-07-05 14:22:58 +1000

    Merged and added functions

--- src/fg/functions.cpp
+++ src/fg/functions.cpp
@@ -2,19 +2,32 @@
 
 #include <cstdlib>
 #include <cmath>
+#include <cassert>
+#include <algorithm>
+
 
 #include <vcg/math/perlin_noise.h>
 
 namespace fg {
-
+        
+    /**
+     * noise in 1, 2 and 3D
+     * 1D and 2D just wrap to 3D noise
+     */
 	double noise(double x){return noise(x,0,0);}
 	double noise(double x, double y){return noise(x,y,0);}
 	double noise(double x, double y, double z){return vcg::math::Perlin::Noise(x,y,z);}
 
+    /*
+     * random convience function uses std::rand() which is not the best RNG
+     */
 	double random(){
 		return (1.0*std::rand())/RAND_MAX; // srand is called when a Universe is created
 	}
-
+    
+    /* return a random in the range [low,high]
+     *
+     */ 
 	double random(double low, double high){
 		return fg::random()*(high-low) + low;
 	}
