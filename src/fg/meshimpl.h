876277dab456305734ed19a56ef5e73b66310c0e
ben <ben@localhost>
2011-07-12 15:02:49 +1000

    Added LICENSE and refs to license in most of the fg source

--- src/fg/meshimpl.h
+++ src/fg/meshimpl.h
@@ -1,11 +1,32 @@
+/**
+ * \file
+ * \brief This header contains the VCG implementation details of a Mesh.
+ * \author ben
+ * 
+ * It is only needed if you are doing low vcg-level manipulations of a mesh.
+ * 
+ * \cond showlicense
+ * \verbatim
+ * --------------------------------------------------------------
+ *    ___     
+ *   |  _|___ 
+ *   |  _| . | fg: real-time procedural 
+ *   |_| |_  | animation and generation 
+ *       |___| of 3D forms
+ *
+ *   Copyright (c) 2011 Centre for Electronic Media Art (CEMA)
+ *   Monash University, Australia. All rights reserved.
+ *
+ *   Use of this software is governed by the terms outlined in 
+ *   the LICENSE file.
+ * 
+ * --------------------------------------------------------------
+ * \endverbatim
+ * \endcond
+ */
+
 #ifndef FG_MESHIMPL_H
 #define FG_MESHIMPL_H
-
-/**
- * This header contains the VCG implementation details of a Mesh.
- * It only needs to be #included in files which do low vcg-level
- * manipulations of a mesh.
- */
 
 #include <vector>
 #include "fg/vec3.h"
--- src/fg/vertex.cpp
+++ src/fg/vertex.cpp
@@ -1,3 +1,26 @@
+/**
+ * \file
+ * 
+ * \cond showlicense
+ * \verbatim
+ * --------------------------------------------------------------
+ *    ___     
+ *   |  _|___ 
+ *   |  _| . | fg: real-time procedural 
+ *   |_| |_  | animation and generation 
+ *       |___| of 3D forms
+ *
+ *   Copyright (c) 2011 Centre for Electronic Media Art (CEMA)
+ *   Monash University, Australia. All rights reserved.
+ *
+ *   Use of this software is governed by the terms outlined in 
+ *   the LICENSE file.
+ * 
+ * --------------------------------------------------------------
+ * \endverbatim
+ * \endcond
+ */
+
 #include "fg/vertex.h"
 #include "fg/meshimpl.h" // VertexImpl
 
