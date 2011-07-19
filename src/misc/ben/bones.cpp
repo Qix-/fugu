5beebace8e57ad58dea8487afd594ff728028a19
ben <ben@localhost>
2011-07-19 16:53:19 +1000

    Updated bones demo

--- src/misc/ben/bones.cpp
+++ src/misc/ben/bones.cpp
@@ -1,5 +1,5 @@
 /**
- * Bones prototype
+ * Joints prototype
  */
 
 #include <iostream>
@@ -40,24 +40,24 @@ void mouseButtoned(int button, int state);
 
 using namespace fg;
 
-/*************** BONES *********************/
+/*************** JointS *********************/
 
-class Bone {
+class Joint {
 	public:
 
-	Bone(Bone* parent = NULL);
+	Joint(Joint* parent = NULL);
 
 	/** \brief recursively calculate world-space transform for self and descendants
 	 *
-	 * The update order for bones is top-down (parent first, then children)
+	 * The update order for Joints is top-down (parent first, then children)
 	 */
 	void update();
 
 	void drawSelfAndChildrenGL();
 
 	// protected:
-	Bone* mParent;
-	std::list<Bone*> mChildren;
+	Joint* mParent;
+	std::list<Joint*> mChildren;
 
 	// relative to parent
 	Vec3 mRelPosition;
@@ -70,16 +70,16 @@ class Bone {
 	Mat4 mWorldSpaceTransform;
 };
 
-Bone::Bone(Bone* parent)
+Joint::Joint(Joint* parent)
 :mParent(parent)
 ,mRelPosition(0,0,0)
-,mRelOrientation()
+,mRelOrientation(Vec3(0,1,0),0)
 ,mWorldSpaceTransform()
 {
 
 }
 
-void Bone::update(){
+void Joint::update(){
 	mRelTransform.setTranslate(mRelPosition);
 	mRelTransform = mRelTransform * mRelOrientation.toMat4();
 
@@ -90,12 +90,12 @@ void Bone::update(){
 		mWorldSpaceTransform = mParent->mWorldSpaceTransform * mRelTransform;
 	}
 
-	BOOST_FOREACH(Bone* b, mChildren){
+	BOOST_FOREACH(Joint* b, mChildren){
 		b->update();
 	}
 }
 
-void Bone::drawSelfAndChildrenGL(){
+void Joint::drawSelfAndChildrenGL(){
 	const float JOINT_RADIUS = .1;
 
 	glColor3f(1,1,1);
@@ -106,8 +106,8 @@ void Bone::drawSelfAndChildrenGL(){
 	GLRenderer::glutSolidSphere(JOINT_RADIUS,8,8);
 	glPopMatrix();
 
-	BOOST_FOREACH(Bone* b, mChildren){
-		// Draw a "bone" between the joints
+	BOOST_FOREACH(Joint* b, mChildren){
+		// Draw a "Joint" between the joints
 		Vec3 start = mWorldSpaceTransform*Vec3(0,0,0);
 		Vec3 end = b->mWorldSpaceTransform*Vec3(0,0,0);
 		GLRenderer::renderBone(start,end);
@@ -115,39 +115,154 @@ void Bone::drawSelfAndChildrenGL(){
 	}
 }
 
-/************************************************/
+class Demo1 {
+public:
+	static const int NUM_JointS = 10;
 
-int DRAW_MODE = 0; // start in flat
-const int NUM_DRAW_MODES = 4;
+	void setup(){
+		root = new Joint();
+		joints[0] = root;
+		Joint* parent = root;
+		double length = 1;
+		for(int i=0;i<NUM_JointS-1;i++){
+			Joint* child = new Joint(parent);
+			child->mRelPosition = Vec3(length,0,0);
+			child->mRelOrientation.set(Vec3(0,0,1),0);
+			parent->mChildren.push_back(child);
+
+			joints[i+1] = child;
+			parent = child;
+			length *= 0.8;
+		}
+	}
+
+	void animate(double time, double dt){
+		for (int i=0;i<NUM_JointS;i++){
+			double di = (double)i/(NUM_JointS-1);
+			double da = (PI/4) * (1.4 - di);
+			double ds = time; // (2*(1+di))*time;
+			joints[i]->mRelOrientation.set(Vec3(0,0,1), da*sin(ds));
+			joints[i]->mRelOrientation.normalise();
+		}
+		root->update();
+	}
+
+	void draw(){
+		//glPushAttrib(GL_LIGHTING_BIT);
+		//glDisable(GL_LIGHTING);
+		glColor3f(1,1,1);
+		root->drawSelfAndChildrenGL();
+		//glPopAttrib();
+	}
+
+	Joint* root;
+	Joint* joints[NUM_JointS];
+};
+
+class Demo2 {
+public:
+	static const int DEPTH = 2;
+	static const int NUM_CHILDREN = 4;
+	static const int NUM_JOINTS = 1+4+16; // computed from the above
+
+	static int _index;
+	void setup(){
+		_index = 0;
+
+		for(int i=0;i<NUM_JOINTS;i++) origAngleF[i] = false;
+
+		root = new Joint();
+		joints[_index++] = root;
+		setupChildren(root, 0, 1);
+	}
+
+	void setupChildren(Joint* parent, int depth, double length){
+		if (depth >= DEPTH) return;
+
+		double da = 2*PI/NUM_CHILDREN;
+
+		for(int i=0;i<NUM_CHILDREN;i++){
+			Joint* child = new Joint(parent);
+
+			Vec3 pos = Vec3(cos(da*i),1,sin(da*i)).normalised();
+			child->mRelPosition = pos*length;
+
+			child->mRelOrientation.set(Vec3(0,1,0),child->mRelPosition);
+			child->mRelOrientation.normalise();
+
+			/*
+			std::cout << "[" << i << "]\n" << child->mRelOrientation << ", ";
+			std::cout << child->mRelOrientation.getAxis() << "\n";
+			*/
+			// std::cout << child->mRelOrientation << "\n";
+
+			parent->mChildren.push_back(child);
+
+			joints[_index++] = child;
+			setupChildren(child, depth+1, length*0.7);
+		}
+	}
+
+	void animate(double time, double dt){
+		for (int i=0;i<NUM_JOINTS;i++){
+			if (not origAngleF[i]){
+				origAngleF[i] = true;
+				axes[i] = joints[i]->mRelOrientation.getAxis();
+				origAngle[i] = joints[i]->mRelOrientation.getAngle();
+				//std::cout << "origAngle[" << i << "] = " << origAngle[i] << "\n";
+			}
+
+			double di = (double)i/(NUM_JOINTS-1);
+			double da = (PI/16); // * (1.4 - di);
+			double ds = time; // (2*(1+di))*time;
+
+			// Vec3 ax = joints[i]->mRelOrientation.getAxis();
+			joints[i]->mRelOrientation.set(axes[i], origAngle[i] + da*sin(ds));
+			//joints[i]->mRelOrientation.normalise();
+
+			if (i>=0 && i<3){
+				//std::cout << "[" << i << "] " << ax << " ## " << origAngle[i] + da*sin(ds) << "\n";
+			}
+		}
+		root->update();
+	}
+
+	void draw(){
+		//glPushAttrib(GL_LIGHTING_BIT);
+		//glDisable(GL_LIGHTING);
+		glColor3f(1,1,1);
+		root->drawSelfAndChildrenGL();
+		//glPopAttrib();
+	}
+
+	Joint* root;
+	Joint* joints[NUM_JOINTS];
+	Vec3 axes[NUM_JOINTS];
+	double origAngle[NUM_JOINTS];
+	bool origAngleF[NUM_JOINTS];
+};
+
+int Demo2::_index = 0;
+
+int DEMO = 0;
+const int NUM_DEMOS = 2;
+
+/************************************************/
 
 void GLFWCALL keyCallback(int key, int action)
 {
 	if (key==GLFW_KEY_TAB and action==GLFW_PRESS){
-		DRAW_MODE = (DRAW_MODE + 1)%NUM_DRAW_MODES;
+		DEMO = (DEMO+1)%NUM_DEMOS;
 	}
 }
 int main(int argc, char *argv[])
 {
 	setupWindowAndGL();
+	std::cout << "[Tab] cycles through demos...\n";
 
-	// Set-up an armature
-	const int NUM_BONES = 20;
-	Bone* bones[NUM_BONES];
-
-	Bone root;
-	bones[0] = &root;
-	Bone* parent = &root;
-	double length = 1;
-	for(int i=0;i<NUM_BONES-1;i++){
-		Bone* child = new Bone(parent);
-		child->mRelPosition = Vec3(length,0,0);
-		child->mRelOrientation.set(Vec3(0,0,1),0);
-		parent->mChildren.push_back(child);
-
-		bones[i+1] = child;
-		parent = child;
-		length *= 0.8;
-	}
+	//
+	Demo1 demo1; demo1.setup();
+	Demo2 demo2; demo2.setup();
 
 	// Run as fast as I can
 	bool running = true;
@@ -161,16 +276,9 @@ int main(int argc, char *argv[])
 		dt = now - time;
 		time = now;
 
-		/** animate bones **/
-
-		for (int i=0;i<NUM_BONES;i++){
-			double di = (double)i/(NUM_BONES-1);
-			double da = (PI/4) * (1.4 - di);
-			double ds = time; // (2*(1+di))*time;
-			bones[i]->mRelOrientation.set(Vec3(0,0,1), da*sin(ds));
-			bones[i]->mRelOrientation.normalise();
-		}
-		root.update();
+		/** animate **/
+		if (DEMO==0) demo1.animate(time,dt);
+		else if (DEMO==1) demo2.animate(time,dt);
 
 		/**/
 
@@ -191,14 +299,8 @@ int main(int argc, char *argv[])
 		glScalef(z,z,z);
 
 		GLRenderer::renderAxes();
-		// Draw stuff here
-		// fg::GLRenderer::renderMesh(&*mesh,fg::GLRenderer::RenderMeshMode(DRAW_MODE));
-
-		//glPushAttrib(GL_LIGHTING_BIT);
-		//glDisable(GL_LIGHTING);
-		glColor3f(1,1,1);
-		root.drawSelfAndChildrenGL();
-		//glPopAttrib();
+		if (DEMO==0) demo1.draw();
+		else if (DEMO==1) demo2.draw();
 
 		glPopMatrix();
 
