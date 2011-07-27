bc4b5fe67487a2deb6f56a5d0e48af656d83aea0
wetterj <wetter.j@gmail.com>
2011-07-28 08:35:55 +1000

    New beady stalk.

--- scripts/cylinders/blastoidea.lua
+++ scripts/cylinders/blastoidea.lua
@@ -2,87 +2,66 @@ module(...,package.seeall)
 
 local m = {}
 local node = {}
-local first = true
 local stalk = {}
 
 function setup()
 	-- The stalk
-   	stalk = new_stalk(1.,1.,.03,.7)
+   	stalk = new_stalk(10,1.,.6,.7)
+
+	m = stalk:getMesh()
+	-- Node for the mesh
+	node = fg.meshnode(m)
+	fgu:add(node)
 end
 
 local t = 0
 function update(dt)
 	t = t + dt
-
-	m = stalk:getMesh()
-	if (first) then
-		-- Node for the mesh
-		node = fg.meshnode(m)
-		fgu:add(node)
-		first = false
-	end
-	node:setMesh(m)
-
-	stalk:update(dt)
 end
 
 function new_stalk(n,width,length,meander)
 	local obj = {
 		numBeads = n,
-		beadWidth = width,
-		beadLeangth = length,
-		meanderParam = meander
+		beadWidth = width * 10,
+		beadLength = length,
+		meanderParam = meander,
+
+		dl = length * .25,
+		dw = width * 9
 	}
 
 	obj.getMesh = function(self)
 	  bert = fg.turtle()
-
-      bert:pushState()
-	  bert:setFrame(fg.vec3(.07,0.,0.),fg.vec3(0.,1.,0.),fg.vec3(0.,0.,1.))
-	  bert:setStiffness(0.3,0.3)
-	  bert:beginCrossSection()
-	  bert:setFrame(fg.vec3(0.,.07,0.),fg.vec3(-1.,0.,0.),fg.vec3(0.,0.,1.))
-	  bert:addPoint()
-	  bert:setFrame(fg.vec3(-.07,0.,0.),fg.vec3(0.,-1.,0.),fg.vec3(0.,0.,1.))
-	  bert:addPoint()
-	  bert:setFrame(fg.vec3(.0,-.07,0.),fg.vec3(1.,0.,0.),fg.vec3(0.,0.,1.))
-	  inner = bert:endCrossSection()
+	  bert:setCarrierMode(0)
 
 	  -- Create the cylinder
-	  bert:popState()
-	  cLength = 0.
-	  bert:setScale(self.baseWidth)
+	  width = self.beadWidth
+	  length = self.beadLength
+	  bert:setScale(self.beadWidth)
 	  bert:beginCylinder()
-	  while (cLength < self.currentLength - self.bumpLength) do
-		cLength = cLength + self.bumpLength
-		bert:setCrossSection(inner)
-	  	bert:move(self.bumpLength)
-		bert:addPoint()
+	  bert:move(length)
+  	  bert:setScale(width)
 
-		cLength = cLength + self.bumpLength
-		bert:setCrossSection(0)
-	  	bert:move(self.bumpLength)
+	  for i = 2, self.numBeads, 1 do
+	  	bert:addPoint()
+
+		bert:move(self.dl * .5)
+		bert:setScale(self.dw)
 		bert:addPoint()
+		bert:move(self.dl * .5)
+
+	  	width = self.beadWidth * fg.random(0.91,1.09)
+	  	length = self.beadLength * fg.random(0.91,1.09)
+		bert:setScale(width)
+		bert:addPoint()
+		bert:move(length)
+		bert:setScale(width)
 	  end
-	  bert:move(self.currentLength - cLength + self.bumpLength)
-	  bert:setScale(self.tipWidth)
 	  bert:endCylinder()
-	  return bert:getMesh(1,150)
+	  return bert:getMesh(1,self.numBeads*5)
 	end
 
 	obj.update = function(self,dt)
-	    self.t = self.t + dt
-		t = fg.clamp(self.t, 0., self.growTimeLength)
-		x = self.t / self.growTimeLength
-		self.currentLength = self.maxLength * fg.smoothstep(0., 1, x)
-
-		t = fg.clamp(self.t, 0., self.growTimeBase)
-		x = self.t / self.growTimeBase
-		self.baseWidth = self.baseWidthInit + self.maxWidth * fg.smoothstep(0., 1., x)
-
-		t = fg.clamp(self.t, 0., self.growTimeTip)
-		x = self.t / self.growTimeTip
-		self.tipWidth = self.tipWidthInit + self.maxWidth * fg.smoothstep(0., 1., x)
 	end
 
 	return obj
