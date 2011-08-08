f32a3232178f86ec5a7c4eb69533b9a21e7693c6
ben <ben@localhost>
2011-08-08 14:57:36 +1000

    texturing!

---
+++ src/fg/ppm.h
@@ -0,0 +1,45 @@
+/* A simple class for encapsulating PPM images 
+ * BP 12.05.05
+ */
+
+#ifndef PPM_H
+#define PPM_H
+
+#include <string>
+#include <cstdio>
+#include <GL/gl.h>
+
+class Ppm
+{
+	public:
+
+	Ppm(std::string filename);
+	~Ppm();
+
+	bool IsValid(){return mIsValid;}
+	int GetWidth(){return mWidth;}
+	int GetHeight(){return mHeight;}
+	
+	GLuint GetGLTex(); //binds the image into a GL texture object
+
+	// Save: Outputs a PPM specified by the parameters (assumes 1 byte per colour component, 3 colours per pixel)
+	static void Save(unsigned char *data, int width, int height, std::string filename);
+	
+	protected:		
+
+	bool mIsValid; //is this image valid? (i.e. loaded)
+
+	std::string mName;	
+	int mWidth, mHeight;
+	int mMaxVal;
+	int mBPC; //bytes per color component
+	void* mData;
+
+
+	/* Helper functions */
+	protected:
+	int _CheckMN(char* mn); //check the magic number
+	void _DiscardComments(std::FILE* fp); //ignore comments
+};
+
+#endif
