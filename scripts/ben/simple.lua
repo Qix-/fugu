e4a02528834aa08c13af903664af65f5b0acd707
ben <ben@localhost>
2011-10-25 14:03:57 +1100

    fugu queries before restarting

--- scripts/ben/simple.lua
+++ scripts/ben/simple.lua
@@ -2,9 +2,9 @@
 	A simple demo -BP	
 --]]
 module(...,package.seeall)
-
+ 
 mesh = require "fgx.mesh"
-
+  
 function vnoise(v)
 	-- return a vec3 of noise
 	return vec3(noise(v.x),noise(v.y),noise(v.z))
@@ -30,7 +30,7 @@ function setup()
 	m:smoothSubdivide(2)
 	local n = fg.meshnode(m)
 	n:setTransform(mat4():setRotateRad(pi/2,vec3(1,0,0)))
-	fgu:add(n)	
+	fgu:add(n)	 
 	
 	op = {}
 	sph = {}
@@ -43,8 +43,9 @@ function update(dt)
 		local th = sph[i][2]
 		local ph = sph[i][3]
 		if (th > pi/2) then
-			local shift = pulser(6*fgu.t+(1+sin(pi*v.p.x))*ph)*1.5*(th-pi/2)/(pi/2)
+			local shift = pulser(3*fgu.t+(1+sin(pi*v.p.x))*ph)*1.5*(th-pi/2)/(pi/2)
 			v.p = op[i] + vec3(0,0,shift)			
-			v:setColour(lerp(vec3(1,1,1),vec3(0,abs(cos(ph)),1),abs(shift/1.5)))			end		
+			v:setColour(lerp(vec3(1,1,1),vec3(0,abs(cos(ph)),1),abs(shift/1.5)))
+		end		
 	end
 end
