a2b1903706516d1d1fb3c2f8c1b50edece16c038
ben <ben@localhost>
2011-10-26 15:10:14 +1100

    few doc fixes

--- core/geometry.lua
+++ core/geometry.lua
@@ -4,5 +4,5 @@ document[[line_exp_point_dist(p1,p2,q) calculates the distance from q to the exp
 document[[segment_point_dist(p1,p2,q) calculates the distance from q to the explicit line segment (p1,p2)]](segment_point_dist)
 
 foreach({line_exp_point_dist,segment_point_dist}, 
-	function(_,v) categorise(v,"geometry") end
+	function(_,v) categorise(v,"geometric") end
 )
